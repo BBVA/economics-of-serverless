d6b5d6ffdbfd3b1e0225a4c4af10c514b7870259
Fernando Alvarez <fer.alvarez@bbva.com>
2018-01-12 20:09:52 +0100

    Use awscosts.EC2 in simulation

--- riot.py
+++ riot.py
@@ -1,33 +1,13 @@
-import math
 import json
-import numpy as np
 import pandas as pd
 
 from datetime import datetime
 from functools import reduce
 from itertools import groupby, chain
-from random import randint
 from datetime import timedelta
 from iot.simulator import *
 
 import awscosts
-
-
-class EC2(object):
-    cost_per_hour = 0
-    max_concurrent_reqs = 0
-    price_per_hour = 0
-
-    def __init__(self, price_per_hour, max_concurrent_reqs):
-
-        self.price_per_hour = float(price_per_hour)
-        self.max_concurrent_reqs = max_concurrent_reqs
-
-    def cost(self, hours):
-        return self.price_per_hour * hours
-
-    def get_n_instances(self, reqs):
-        return math.ceil(reqs / self.max_concurrent_reqs)
 
 
 class Model(object):
@@ -49,13 +29,18 @@ class Model(object):
         self.ec2_instance = ec2_instance
 
 
-def new_model(num_devices, request_period, num_days, resolution, ec2_price_per_hour, ec2_max_concurrent_reqs, lambda_mb_per_req, lambda_ms_per_req):
+def new_model(num_devices, request_period, num_days, resolution,
+              ec2_flavor, lambda_mb_per_req, lambda_ms_per_req):
+
     today = datetime.today()
     period_start = datetime(today.year, 1, 1)
     period_end = period_start + timedelta(days=num_days)
     lambda_instance = awscosts.Lambda(lambda_mb_per_req, lambda_ms_per_req)
-    ec2_instance = EC2(ec2_price_per_hour, ec2_max_concurrent_reqs)
-    return Model(num_devices, request_period, period_start, period_end, resolution, lambda_instance, ec2_instance)
+    ec2_instance = awscosts.EC2(ec2_flavor, MB_per_req=lambda_mb_per_req,
+                                ms_per_req=lambda_ms_per_req)
+
+    return Model(num_devices, request_period, period_start, period_end,
+                 resolution, lambda_instance, ec2_instance)
 
 
 def generate(model):
@@ -93,10 +78,10 @@ def costs(model, df):
     def myl(x):
         nonlocal acc
         reqs = x['hits']
-        hours = model.resolution_in_seconds / 3600
         lc = model.lambda_instance.get_cost(acc, reset_free_tier=True)
-        ni = max(1, model.ec2_instance.get_n_instances(reqs))
-        ec = model.ec2_instance.cost(hours) * ni
+        ni = max(1, model.ec2_instance.get_num_instances(
+            reqs/model.resolution_in_seconds))
+        ec = model.ec2_instance.get_cost(model.resolution_in_seconds) * ni
         acc = acc + x['hits']
         return x['date'], x['hits'], acc, lc, ec, ni
 
@@ -108,17 +93,6 @@ def costs(model, df):
         'ec2_cost', 'instances']] = df.apply(myl, axis=1)
 
     return df
-
-
-def get_ec2_prices():
-    resource_path = '/'.join(('awscosts', 'awscosts', 'ec2prices.json'))
-    # data[instance_type]['memory']
-    # data[instance_type]['hourly_price']
-    return json.load(open(resource_path))
-
-
-def get_ec2_max_concurrent_reqs(flavour, mb_per_req, res):
-    return float(flavour['memory']) * 1024 / mb_per_req * res
 
 
 class DateTimeEncoder(json.JSONEncoder):
@@ -134,23 +108,23 @@ class DateTimeEncoder(json.JSONEncoder):
 
 
 def main():
-    ec2_instances = get_ec2_prices()
     req_freq_list = [3600, 8 * 3600, 24 * 3600]
     device_count_list = [10, 100, 1000, 10000, 100000,
                          1000000, 10000000, 100000000, 1000000000, 10000000000]
     print("df_len,hits,lambda,ec2,freq,instance_count")
     for freq in req_freq_list:
         for device_count in device_count_list:
-            instances = (
-                ec2_instances['t2.large'], ec2_instances['m4.large'], ec2_instances['m4.4xlarge'])
+            flavors = ('t2.large', 'm4.large', 'm4.4xlarge')
             resolution_in_seconds = 60
             period_in_days = 30
             lambda_memory = 128
             request_duration_ms = 200
-			
-            for instance in instances:
-                max_concurrent_requests = get_ec2_max_concurrent_reqs(instance, lambda_memory, resolution_in_seconds)
-                model = new_model(device_count, freq, period_in_days, resolution_in_seconds, instance['hourly_price'], max_concurrent_requests, lambda_memory, request_duration_ms)
+
+            for flavor in flavors:
+                model = new_model(device_count, freq, period_in_days,
+                                  resolution_in_seconds,
+                                  flavor, lambda_memory,
+                                  request_duration_ms)
                 # print(json.dumps(model.__dict__,
                 #                cls=DateTimeEncoder, indent=2, sort_keys=True))
 
@@ -160,7 +134,6 @@ def main():
                          df["ec2_cost"].sum(), freq, df['instances'].max()]
 
                 print(','.join(map(str, result)))
-				
                 # print(df.tail())
 
 

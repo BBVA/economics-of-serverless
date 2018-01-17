0cfdaabcbf4a5c99e21288f6eac2239cd636c1ba
martin.evgeniev <martin.evgeniev@bbva.com>
2018-01-17 14:52:25 +0100

    Automate Jupyter execution.
    Include Jupyter management with eos script to avoid manualy installind
    awscosts library..

--- scripts/eos.sh
+++ scripts/eos.sh
@@ -7,9 +7,30 @@ action="$1"
 function usage {
   echo "USAGE: ${0##*/} <command>"
   echo "Commands:"
-  echo -e "\tbuild\t Build the app"
-  echo -e "\triot\t Execute riot simulator"
-  echo -e "\trun\t Run custom command inside EoS container"
+  echo -e "\tbuild\t         Build the app"
+  echo -e "\triot\t         Execute riot simulator"
+  echo -e "\trun\t         Run custom command inside EoS container"
+  echo -e "\tstart-jupyter\t Start Jupyter local server"
+  echo -e "\tstart-jupyter\t Stop Jupyter local server"
+}
+
+function run_riot {
+  docker run -it --rm -v $PWD:/tmp/execution -w /tmp/execution local/eos:latest
+}
+
+function run_custom_cmd {
+  shift
+  docker run -it --rm -v $PWD:/tmp/execution -w /tmp/execution local/eos:latest "$@"
+}
+
+function start_jupyter {
+  docker run -d --rm --name "${APP_NAME}-jupyter" -p 8888:8888 -v $PWD:/home/jovyan/work jupyter/datascience-notebook
+  docker exec -it "${APP_NAME}-jupyter" pip install -e work/awscosts
+  docker logs "${APP_NAME}-jupyter"
+}
+
+function stop_jupyter {
+  docker rm -vf eos-jupyter
 }
 
 case "$action" in
@@ -19,11 +40,19 @@ case "$action" in
   ;;
   riot)
     echo -e "Running ${APP_NAME} riot.py.."
-    docker run -it --rm -v $PWD:/tmp/execution -w /tmp/execution local/eos:latest
+    run_riot
   ;;
   run)
-    shift
-    docker run -it --rm -v $PWD:/tmp/execution -w /tmp/execution local/eos:latest "$@"
+    echo -e "Running ${APP_NAME} custom cmd"
+    run_custom_cmd "$@"
+  ;;
+  start-jupyter)
+    echo -e "Starting ${APP_NAME} Jupyter"
+    start_jupyter
+  ;;
+  stop-jupyter)
+    echo -e "Stopping ${APP_NAME} Jupyter"
+    stop_jupyter
   ;;
   *)
     usage

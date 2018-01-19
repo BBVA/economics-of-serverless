b5eb5be38b3b4fd7cca8dceaa7e402180365f152
Alvaro Alda <alvaro.alda@bbva.com>
2018-01-19 12:40:11 +0100

    Add plots for the fist IoT scenario

--- riot.py
+++ riot.py
@@ -37,6 +37,7 @@ def aggregate_costs(req_period, resolution, interval_duration, num_devices, lamb
     costs = {'resolution_buckets':0, 'hits':0, 'lambda':0}
     costs['resolution_buckets'] = interval_duration // resolution
     costs['hits'] = num_devices * (interval_duration // req_period)
+    costs['reqs_per_second'] = costs['hits'] / interval_duration
     costs['lambda'] = lambda_instance.get_cost(costs['hits'], reset_free_tier=True)
 
     def calc_ec2_instance_use(num_hits_list, ec2_instance = None, resolution = 0):
@@ -61,62 +62,51 @@ def draw_costs_by_num_devices(costs, num_devices_list, ec2_flavors, req_period):
     data = []
 
     lambda_trace = go.Scatter(
-        x=num_devices_list,
+        x=[cost["reqs_per_second"] for cost in costs],
         y=[cost['lambda'] for cost in costs],
         name='Lambda'
     )
     data.append(lambda_trace)
 
     for flavor in ec2_flavors:
-        trace = go.Scatter(
+        trace1 = go.Scatter(
+            x=[cost["reqs_per_second"] for cost in costs],
+            y=[cost[f'ec2_{flavor}_cost'] * cost[f'ec2_{flavor}_instances'] for cost in costs],
+            name=f'EC2 {flavor}',
+            showlegend=False
+        )
+        trace2 = go.Scatter(
             x=num_devices_list,
             y=[cost[f'ec2_{flavor}_cost'] * cost[f'ec2_{flavor}_instances'] for cost in costs],
-            name=f'EC2 {flavor}'
+            name=f'EC2 {flavor}',
+            xaxis='x2'
         )
-        data.append(trace)
+        data.append(trace1)
+        data.append(trace2)
 
     layout = go.Layout(
         title=f'Cost by number of devices (request period: {req_period} seconds)',
-        legend=dict(orientation="h"),
-        xaxis=dict(
-            title='Number of devices',
-            type='log',
-            autorange=True
+        legend=dict(orientation='h'),
+        width=1000,
+        height=800,
+        margin=go.Margin(
+            t=200,
+            b=200,
+            r=100,
+            l=100,
+            autoexpand=False
         ),
-        yaxis=dict(
-            title='Cost ($)'
-        )
-    )
-
-    fig = go.Figure(data=data, layout=layout)
-    plotly.offline.plot(fig)
-
-def draw_costs_by_num_reqs(costs, ec2_flavors, req_period):
-
-    data = []
-
-    lambda_trace = go.Scatter(
-        x=[cost['hits'] for cost in costs],
-        y=[cost['lambda'] for cost in costs],
-        name='Lambda'
-    )
-    data.append(lambda_trace)
-
-    for flavor in ec2_flavors:
-        trace = go.Scatter(
-            x=[cost['hits'] for cost in costs],
-            y=[cost[f'ec2_{flavor}_cost'] * cost[f'ec2_{flavor}_instances'] for cost in costs],
-            name=f'EC2 {flavor}'
-        )
-        data.append(trace)
-
-    layout = go.Layout(
-        title=f'Cost by number of requests (request period: {req_period} seconds)',
-        legend=dict(orientation="h"),
         xaxis=dict(
-            title='Number of reqs/second',
+            title='Number of reqs/sec',
             type='log',
-            autorange=True
+            anchor='free',
+            position=0.02
+        ),
+        xaxis2=dict(
+            title="Number of devices",
+            type='log',
+            side='top',
+            overlaying='x'
         ),
         yaxis=dict(
             title='Cost ($)'
@@ -140,7 +130,7 @@ def main():
     #[10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000]
     num_devices_list = reduce(devices_func, range(1,13), [10])
 
-    resolution = 60 # in seconds
+    resolution = 60 * 5 # in seconds
     interval_duration = 30 * 24 * 3600 # one month in seconds
     lambda_memory = 128
     lambda_request_duration_ms = 200
@@ -158,7 +148,6 @@ def main():
             costs.append(cost)
 
         draw_costs_by_num_devices(costs, num_devices_list, ec2_flavors, req_period)
-        draw_costs_by_num_reqs(costs, ec2_flavors, req_period)
 
 if __name__ == "__main__":
     main()

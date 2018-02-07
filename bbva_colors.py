592fc7d5243407e33e7a630347d5e84a53393014
Fernando Alvarez <fer.alvarez@bbva.com>
2018-02-07 17:30:19 +0100

    Move BBVA colors definition to a separate file
    Improves reusability (see the future)

---
+++ bbva_colors.py
@@ -0,0 +1,16 @@
+# Colors and styles:
+BBVAcolors = {
+    'core': {'color': '#072146'},
+    'navy': {'color': '#004481'},
+    'aqua': {'color': '#2dcccd'},
+    'medium': {'color': '#2A86CA'},
+    'light': {'color': '#5BBEFF'},
+    'red': {'color': '#da3851'},
+    'coral': {'color': '#f35e61'},
+    'orange': {'color': '#f7893b'},
+    'green': {'color': '#48ae64'},
+    'yellow': {'color': '#f8cd51'},
+    'pink': {'color': '#f78be8'},
+    'gold': {'color': '#d8be75'},
+    'purple': {'color': '#8f7ae5'},
+}

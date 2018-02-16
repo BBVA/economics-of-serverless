625d8edeffc717e437781e9b7099602ac73259ea
Fernando Alvarez <fer.alvarez@bbva.com>
2018-02-16 12:04:48 +0100

    Beautify riot.1.py (make it linter-compliant)

--- riot.1.py
+++ riot.1.py
@@ -13,40 +13,45 @@
 # limitations under the License.
 
 from functools import reduce, partial
-from itertools import accumulate
-from iot.simulator import *
-from random import randint
 import plotly
-import plotly.plotly as py
 import plotly.graph_objs as go
-import numpy as np
 import awscosts
 from bbva_colors import BBVAcolors
 
 
-def generate_requests_time_series(num_devices, request_period, interval_duration, resolution):
-    mean = num_devices // request_period
-    result = list()
-    for i in range(0,(interval_duration//resolution)):
-        if i > 0 and i < 1000:
-            result.append(mean/2)
-        if i >= 1000 and i < 1500:
-            result.append(mean*2)
-        if i >=1500 and i < 6500:
-            result.append(mean/2)
-        if i >= 6500 and i <8000:
-            result.append(mean*2)
-        if i >=8000:
-            result.append(mean/2)
-    return result
-    #return [num_devices // request_period] * (interval_duration//resolution)
+def aggregate_costs(
+    req_period,
+    resolution,
+    interval_duration,
+    num_devices,
+    lambda_instance,
+    ec2_instances_list,
+    # devices_time_series,
+):
 
-def aggregate_costs(req_period, resolution, interval_duration, num_devices, lambda_instance, ec2_instances_list, devices_time_series):
-    costs = {'resolution_buckets':0, 'hits':0, 'lambda':0}
+    # Generate Time Series:
+    mean = num_devices // req_period
+    time_series = list()
+    for i in range(0, (interval_duration // resolution)):
+        if i > 0 and i < 1000:
+            time_series.append(mean / 2)
+        if i >= 1000 and i < 1500:
+            time_series.append(mean * 2)
+        if i >= 1500 and i < 6500:
+            time_series.append(mean / 2)
+        if i >= 6500 and i < 8000:
+            time_series.append(mean * 2)
+        if i >= 8000:
+            time_series.append(mean / 2)
+
+    costs = {}
     costs['resolution_buckets'] = interval_duration // resolution
     costs['hits'] = num_devices * (interval_duration // req_period)
     costs['reqs_per_second'] = costs['hits'] / interval_duration
-    costs['lambda'] = lambda_instance.get_cost(costs['hits'], reset_free_tier=True)
+    costs['lambda'] = lambda_instance.get_cost(
+        costs['hits'],
+        reset_free_tier=True
+    )
 
     def calc_ec2_instance_use(num_hits, ec2_instance, resolution=0):
         return ec2_instance.get_cost_and_num_instances(
@@ -70,13 +75,19 @@ def aggregate_costs(req_period, resolution, interval_duration, num_devices, lamb
         )
 
         costs.update({
-            f'ec2_{ec2_instance._instance_type}_cost':cost,
+            f'ec2_{ec2_instance._instance_type}_cost': cost,
             f'ec2_{ec2_instance._instance_type}_instances': instances
-            })
+        })
 
     return costs
 
-def draw_costs_by_num_devices(costs, num_devices_list, ec2_flavors, req_period):
+
+def draw_costs_by_num_devices(
+    costs,
+    num_devices_list,
+    ec2_flavors,
+    req_period
+):
 
     data = []
 
@@ -98,8 +109,9 @@ def draw_costs_by_num_devices(costs, num_devices_list, ec2_flavors, req_period):
         data.append(trace)
 
     layout = go.Layout(
-        title=f'<b>Monthly cost by number of requests per second</b><br><i>(request period: {req_period} seconds)</i></b>',
-        legend=dict(orientation='v'),
+        title=f'<b>Monthly cost by number of requests per second</b><br>' +
+        f'<i>(request period: {req_period} seconds)</i></b>',
+        legend=dict(orientation='h'),
         width=1000,
         height=800,
         xaxis=dict(
@@ -115,6 +127,7 @@ def draw_costs_by_num_devices(costs, num_devices_list, ec2_flavors, req_period):
     fig = go.Figure(data=data, layout=layout)
     plotly.offline.plot(fig)
 
+
 def main():
 
     def devices_func(acc, i):
@@ -125,28 +138,55 @@ def main():
         return acc
 
     ec2_flavors = ('m3.medium', 'm4.large', 'm4.4xlarge')
-    req_period_list = [3600] # , 8* 3600, 24 * 3600]
-    #[10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000]
-    num_devices_list = reduce(devices_func, range(1,13), [10])
-    num_devices_list = list(range(200000,10000000,100000))
-    resolution = 60 * 5 # in seconds
-    interval_duration = 30 * 24 * 3600 # one month in seconds
+    req_period_list = [3600]  # , 8* 3600, 24 * 3600]
+    # [10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000,
+    # 5000000, 10000000]
+    num_devices_list = reduce(devices_func, range(1, 13), [10])
+    num_devices_list = list(range(200000, 10000000, 100000))
+    resolution = 60 * 5  # in seconds
+    interval_duration = 30 * 24 * 3600  # one month in seconds
     lambda_memory = 128
     lambda_request_duration_ms = 200
-    ec2_instances = [awscosts.EC2(flavor, MB_per_req=lambda_memory, ms_per_req=lambda_request_duration_ms) for flavor in ec2_flavors]
+    ec2_instances = [awscosts.EC2(
+        flavor,
+        MB_per_req=lambda_memory,
+        ms_per_req=lambda_request_duration_ms
+    ) for flavor in ec2_flavors]
 
     for req_period in req_period_list:
         costs = []
         for num_devices in num_devices_list:
-            lambda_instance = awscosts.Lambda(lambda_memory, lambda_request_duration_ms)
-            time_series = generate_requests_time_series(num_devices, req_period, interval_duration, resolution)
-            cost = aggregate_costs(req_period, resolution, interval_duration, num_devices, lambda_instance, ec2_instances, time_series)
+            lambda_instance = awscosts.Lambda(
+                lambda_memory,
+                lambda_request_duration_ms
+            )
+            cost = aggregate_costs(
+                req_period,
+                resolution,
+                interval_duration,
+                num_devices,
+                lambda_instance,
+                ec2_instances,
+            )
 
-            print(cost['hits'], cost['reqs_per_second'], cost['lambda'], cost['ec2_m3.medium_cost'] * cost[f'ec2_m3.medium_instances'], cost['ec2_m4.large_cost'] * cost[f'ec2_m4.large_instances'], cost['ec2_m4.4xlarge_cost']*cost[f'ec2_m4.4xlarge_instances'])
+            print(
+                cost['hits'],
+                cost['reqs_per_second'],
+                cost['lambda'],
+                cost['ec2_m3.medium_cost'] * cost[f'ec2_m3.medium_instances'],
+                cost['ec2_m4.large_cost'] * cost[f'ec2_m4.large_instances'],
+                cost['ec2_m4.4xlarge_cost'] * cost[f'ec2_m4.4xlarge_instances']
+            )
 
             costs.append(cost)
 
-        draw_costs_by_num_devices(costs, num_devices_list, ec2_flavors, req_period)
+        draw_costs_by_num_devices(
+            costs,
+            num_devices_list,
+            ec2_flavors,
+            req_period
+        )
+
 
 if __name__ == "__main__":
     main()
